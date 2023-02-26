5b8027f48ed0059e93315e92a1c9cdec3bceffe3
gopikrishnannm <gopikrishnannm01@gmail.com>
2023-02-26 15:05:00 +0530

    second commit

---
+++ TestNGProject/src/test/java/com/tng/Tests/FaceboolLoginTest.java
@@ -0,0 +1,45 @@
+package com.tng.Tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FaceboolLoginTest {
+	
+	WebDriver driver;
+	
+	@Test
+	public void launchFbApp() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+	}
+	
+	@Test
+	public void launchGoogleApp() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+	}
+	
+	@Test
+	public void testFacebookLogin() throws InterruptedException {
+		driver.findElement(By.name("email")).sendKeys("username1", Keys.ENTER);
+		System.out.println(driver.getTitle());
+		Thread.sleep(500);
+		
+	}
+	
+	@Test
+	public void warningPopUp() {
+		boolean isDisplayed = driver.findElement(By.className("_9ay7")).isDisplayed();
+		System.out.println(isDisplayed == true ? "Login Successfull " : "Login Failed");
+	}
+	
+}
